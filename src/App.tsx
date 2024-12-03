bb7725499626030bf057bba8fc3def88746a6ac4
amirbekk7 <normurodovamirbek3@gmail.com>
2024-12-03 23:37:25 +0500

    tailwand,mui install buldi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-// import ChangePassword from "./auth/ChangePasswordForm/changePassword"
-// import Login from "./auth/LoginForm/login"
-// import Register from "./auth/RegisterForm/register"
+//  FONTLAR MUI
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import ChangePassword from "./auth/ChangePasswordForm/changePassword"
+import Login from "./auth/LoginForm/login"
+import Register from "./auth/RegisterForm/register"
 
 function App() {
   return (
     <>
-     {/* <Login /> 
+     <Login /> 
      <Register />
-     <ChangePassword/> */}
+     <ChangePassword/>
     </>
   )
 }
