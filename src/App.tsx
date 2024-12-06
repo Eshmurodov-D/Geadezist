e5b7845775131ee5bb4ba5b1012e737ff8bb4011
Nothing <sardorbekeshmatov79@gmail.com>
2024-12-06 11:40:30 +0500

    TAyyor

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 //  FONTLAR MUI
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import ChangePassword from "./auth/ChangePasswordForm/changePassword"
-import Login from "./auth/LoginForm/login"
-import Register from "./auth/RegisterForm/register"
-import { Route,  Routes } from 'react-router-dom';
+import ChangePassword from "./auth/ChangePasswordForm/changePassword";
+import Login from "./auth/LoginForm/login";
+import Register from "./auth/RegisterForm/register";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import Navbar from "./Components/Navigations/navbar";
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        <ChangePassword/>
+          <Route path="/" element={<Navbar />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/changePassword" element={<ChangePassword />} />
         </Routes>
-
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
