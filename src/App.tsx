d59dbbfad37976458d947703f6ba5723771ce554
Iba <ibrohim@gmail.com>
2024-12-06 16:47:31 +0300

    ishlasin

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-//  FONTLAR MUI
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
-import ChangePassword from "./auth/ChangePasswordForm/changePassword";
-import Login from "./auth/LoginForm/login";
-import Register from "./auth/RegisterForm/register";
-import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
-import Navbar from "./Components/Navigations/navbar";
-
-function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Navbar />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/changePassword" element={<ChangePassword />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
-
-export default App;
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Register from "./auth/RegisterForm/register";
 import ChangePassword from "./auth/ChangePasswordForm/changePassword";
@@ -47,4 +17,3 @@ function App() {
     </BrowserRouter>
   );}
  export default App
->>>>>>> 1fba7f4f3ec7779c47b4adfb33b1414cbf790bcb
