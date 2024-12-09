fa31daf3300202db7593e2d1212cd0dbbba3ffcd
dilbarqoldosheva <bintushokir07@gmail.com>
2024-12-09 17:39:18 +0500

    add

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
         <Navbar />
 
-<<<<<<< HEAD
+
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/changepass" element={<ChangePassword />} />
@@ -28,17 +28,7 @@ function App() {
 
       </Routes>
     </BrowserRouter>
-=======
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/changepass" element={<ChangePassword />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/ververify-email" element={<VerifyEmail />} />
-          <Route path="/EmployeeTable" element={<EmployeeTable />} />
-          {/* <Route path="/nav" element={<Navbar/>} /> */}
-        </Routes>
-      </BrowserRouter>
->>>>>>> 5403899bbbba2beb3a337e8ac67ee6a55318606e
+
     </>
   );
 }
