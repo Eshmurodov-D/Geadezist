c97070e3d1f2ae7836c961a05eb5b2c91b8c30d6
azizbekanvarov <azizbekanvarov321@gmail.com>
2024-12-09 16:29:11 +0500

    skhdgcbn

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 //  FONTLAR MUI
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import ChangePassword from "./auth/ChangePasswordForm/changePassword"
-import Login from "./auth/LoginForm/login"
-import Register from "./auth/RegisterForm/register"
-import { Route,  Routes } from 'react-router-dom';
+import ChangePassword from "./auth/ChangePasswordForm/changePassword";
+import Login from "./auth/LoginForm/login";
+import Register from "./auth/RegisterForm/register";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-        <Routes>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        <ChangePassword/>
-        </Routes>
-
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <ChangePassword />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
